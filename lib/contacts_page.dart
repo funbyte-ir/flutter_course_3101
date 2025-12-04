9a97a0e74e663470060d0c131b5ba46f691f978c
Sayed Mohammad Amin Emrani <smae1993@gmail.com>
2025-12-04 17:59:37 +0330

    working on lesson 4

--- lib/contacts_page.dart
+++ lib/contacts_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/contact.dart';
 
 class ContactsPage extends StatefulWidget {
   @override
@@ -6,8 +7,79 @@ class ContactsPage extends StatefulWidget {
 }
 
 class _ContactsPageState extends State<ContactsPage> {
+  List<Contact> contacts = [
+    Contact("Ali", "09136336002"),
+    Contact("Mohammad", "09136336003"),
+    Contact("Hassan", "09136336004"),
+  ];
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController mobileController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("Contact App")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Add Contact"),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(hint: Text("Name")),
+                    ),
+                    TextField(
+                      controller: mobileController,
+                      decoration: InputDecoration(hint: Text("Mobile")),
+                    ),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Contact newContact = Contact(
+                        nameController.text,
+                        mobileController.text,
+                      );
+
+                      setState(() {
+                        contacts.add(newContact);
+                        mobileController.clear();
+                        nameController.clear();
+                      });
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Add"),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+      body: ListView(
+        children: contacts
+            .map((e) => ListTile(title: Text(e.name), trailing: Text(e.mobile)))
+            .toList(),
+      ),
+    );
+  }
+
+  List<Widget> convertToWidget() {
+    List<Widget> lst = [];
+    for (var contact in contacts) {
+      //
+      lst.add(
+        ListTile(title: Text(contact.name), trailing: Text(contact.mobile)),
+      );
+    }
+    return lst;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/contacts_page.dart';
 // import 'package:flutter_application_1/calculator.dart';
 import 'package:flutter_application_1/game_page.dart';
+import 'package:flutter_application_1/home_page_stateful.dart';
 // import 'package:flutter_application_1/home_page.dart';
-import 'package:flutter_application_1/splash_page.dart';
+// import 'package:flutter_application_1/splash_page.dart';
 import 'package:flutter_application_1/suduku_page.dart';
 
 void main() {
@@ -12,7 +14,7 @@ void main() {
       debugShowCheckedModeBanner: false,
       initialRoute: "/",
       routes: {
-        "/": (context) => SplashPage(),
+        "/": (context) => ContactsPage(),
         "/game": (context) => GamePage(),
         "/suduku": (context) => SudukuPage(),
       },
